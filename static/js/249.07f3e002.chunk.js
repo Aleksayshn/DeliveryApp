a5cbca26b93675fcc4b31e422f59fd7be8d70a35
Aleksayshn <ag7756862@gmail.com>
2023-05-31 02:13:58 +0000

    Deploying to gh-pages from @ Aleksayshn/DeliveryApp@6384faa455e26ac106c0d143f53cc97ddd553092 🚀

---
+++ static/js/249.07f3e002.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[249],{7249:function(e,s,a){a.r(s);var n=a(4361),c=a(184);s.default=function(){return(0,c.jsx)("div",{style:{marginTop:"5rem"},className:"MockAPI",children:(0,c.jsx)(n.Z,{})})}}}]);
+//# sourceMappingURL=249.07f3e002.chunk.js.map
